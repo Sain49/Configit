e909020c7bda17ad307bc4a6826d977cba73d188
Arif.A <arif.sain@gmail.com>
2025-08-16 12:58:41 +0200

    Add IsValid method to Resolver for conflict detection
    Added IsValid to Resolver.cs to verify if initial packages and their dependencies can be resolved without version conflicts. The method checks for multiple required versions of the same package and returns false if any conflict is found.

--- ResolveDependencies/Resolver.cs
+++ ResolveDependencies/Resolver.cs
@@ -8,6 +8,32 @@ namespace ResolveDependencies
 {
     public class Resolver
     {
+        public bool IsValid(List<Package> initialPackages, Dictionary<Package, List<Package>> dependencies)
+        {
+            var requiredPackages = new Dictionary<string, string>();
+            var packagesToProcess = new Queue<Package>(initialPackages);
 
+            while (packagesToProcess.Count > 0)
+            {
+                var currentPackage = packagesToProcess.Dequeue();
+
+                if (requiredPackages.TryGetValue(currentPackage.Name, out var requiredVersion)){
+                    if (requiredVersion != currentPackage.Version) 
+                        return false; // conflict detected
+                }
+                else
+                {
+                    requiredPackages.Add(currentPackage.Name, currentPackage.Version);
+
+                    if (dependencies.TryGetValue(currentPackage, out var packageDependencies))
+                    {
+                        foreach (var dependency in packageDependencies) 
+                            packagesToProcess.Enqueue(dependency);
+                    }
+                }
+            }
+
+            return true;
+        }
     }
 }
