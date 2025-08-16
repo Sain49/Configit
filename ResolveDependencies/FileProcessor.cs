32a42f6614a202f8e9bab06b5205ca970f80ac8d
Arif.A <arif.sain@gmail.com>
2025-08-16 13:57:51 +0200

    Implement ProcessFile to parse and validate dependencies

--- ResolveDependencies/FileProcessor.cs
+++ ResolveDependencies/FileProcessor.cs
@@ -30,7 +30,48 @@ namespace ResolveDependencies
 
         private void ProcessFile(string inputFilePath)
         {
-           throw new NotImplementedException();
+            var lines = File.ReadAllLines(inputFilePath);
+            int lineIndex = 0;
+
+            if (lines.Length == 0) return;
+
+            int numPackages = int.Parse(lines[lineIndex++]);
+            var initialPackages = new List<Package>();
+            for (int i = 0; i < numPackages; i++) { 
+                if (lineIndex >= lines.Length) break;
+                var line = lines[lineIndex++];
+
+                if(string.IsNullOrEmpty(line)) continue;
+                var parts = line.Split(',');
+                initialPackages.Add(new Package(parts[0], parts[1]));
+             }
+
+            if (lineIndex >= lines.Length)
+            {
+                // no dependencies defined
+            }
+
+            int numDependencies = (lineIndex >= lines.Length) ? 0 : int.Parse(lines[lineIndex++]);
+            var dependencies = new Dictionary<Package, List<Package>>();   
+            for (int i = 0;i < numDependencies;i++) {
+                if (lineIndex >= lines.Length) break;
+                var line = lines[lineIndex++];
+                if (string.IsNullOrEmpty(line)) continue;
+                var parts = line.Split(',');
+                var package = new Package(parts[0], parts[1]);
+                var dependency = new Package(parts[2], parts[3]);
+
+                if (!dependencies.ContainsKey(package)){ 
+                    dependencies[package] = new List<Package>(); 
+                }
+                dependencies[package].Add(dependency);
+             }
+
+            bool isValid = _resolver.IsValid(initialPackages, dependencies);
+
+            var outputFilePath = Path.ChangeExtension(inputFilePath, ".out.txt");
+            var result = isValid ? "PASS" : "FAILED";
+            File.WriteAllText(outputFilePath, result);
         }
     }
 }
