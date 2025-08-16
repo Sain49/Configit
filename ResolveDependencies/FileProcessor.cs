911cbe43fcac7988b14df1d10cd1a2463cb046ad
Arif.A <arif.sain@gmail.com>
2025-08-16 13:40:41 +0200

    Expand FileProcessor with Resolver and directory support

--- ResolveDependencies/FileProcessor.cs
+++ ResolveDependencies/FileProcessor.cs
@@ -9,5 +9,28 @@ namespace ResolveDependencies
 {
     public class FileProcessor
     {
+        private readonly Resolver _resolver;
+        public FileProcessor(Resolver resolver) { _resolver = resolver; }
+
+        public void ProcessDirectory(string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                Console.WriteLine($"Input directory not found: {directoryPath}");
+                return;
+            }
+
+            var inputFiles = Directory.GetFiles(directoryPath, "*.txt")
+                .Where(file => !file.EndsWith(".out.txt"));
+
+            foreach (var inputFile in inputFiles)
+                ProcessFile(inputFile);
+
+        }
+
+        private void ProcessFile(string inputFilePath)
+        {
+           throw new NotImplementedException();
+        }
     }
 }
